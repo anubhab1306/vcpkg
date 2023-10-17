178a9374dbeac8ee7d89e4c4a12610e159acdbc4
Alexander Vieth <a.vieth@tudelft.nl>
2023-10-17 15:41:40 +0200

    Update Eigen3 version

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -4,12 +4,11 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF 3.4.0
-    SHA512 ba75ecb760e32acf4ceaf27115468e65d4f77c44f8d519b5a13e7940af2c03a304ad433368cb6d55431f307c5c39e2666ab41d34442db3cf441638e51f5c3b6a
-    HEAD_REF master
+    REF 3.4.1
+    SHA512 7176ae16238ded7fb5ed30a7f5215825b3abd134
+    HEAD_REF 3.4.1
     PATCHES
         remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
-        fix-vectorized-reductions-half.patch # Remove this patch in the next update
 )
 
 vcpkg_cmake_configure(
